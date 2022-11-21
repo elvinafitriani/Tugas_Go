fefbee38cc65885e33b446e72c7aea14e76f98b9
putrilest <putriles203@gmail.com>
2022-11-21 09:41:51 +0700

    TugasPuput

---
+++ connection/db.go
@@ -0,0 +1,22 @@
+package connection
+
+import (
+	"belajargorm/models"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectToDb() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/toko"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.Kategori{}, &models.Barang{}, &models.Jual{})
+
+	return db
+}
